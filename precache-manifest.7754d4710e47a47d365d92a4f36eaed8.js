c037a62f4763226b0668234a6bb45bdcbfc633b8
Rahul Arora <r.arora@alientt.com>
2019-04-01 15:45:23 +0530

    Updates

---
+++ precache-manifest.7754d4710e47a47d365d92a4f36eaed8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0280ccd7ee9cee4f49e",
+    "url": "/rahularorar95/CourseAdda/static/css/main.03f1dac4.chunk.css"
+  },
+  {
+    "revision": "b0280ccd7ee9cee4f49e",
+    "url": "/rahularorar95/CourseAdda/static/js/main.57eae111.chunk.js"
+  },
+  {
+    "revision": "c89ac66597e5674dce96",
+    "url": "/rahularorar95/CourseAdda/static/js/runtime~main.aa01ca50.js"
+  },
+  {
+    "revision": "a4c724711c1ca9b29c00",
+    "url": "/rahularorar95/CourseAdda/static/js/2.4a45a091.chunk.js"
+  },
+  {
+    "revision": "76eae1ca0e5bae397ba711df457d0166",
+    "url": "/rahularorar95/CourseAdda/static/media/loader.76eae1ca.gif"
+  },
+  {
+    "revision": "890825038d0a9a0e81c6dd63ed358d64",
+    "url": "/rahularorar95/CourseAdda/index.html"
+  }
+];
